6af4a524490be39d4bdb29ff9d1980452118e6e2
Abraham <abrahamriveracho2019@gmail.com>
2026-09-12 21:49:27 -0500

    Primeras configuracion del proyecto de react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+   <div className='contenedor-principal'>
+    <div className='title'>
+      <img/>
+      <h1> Coding Conf</h1>
+    </div>
+   </div>
+  );
+}
+
+export default App
